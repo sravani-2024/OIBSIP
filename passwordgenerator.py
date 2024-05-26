c62ee485f3963b11f2a2031070d3a7e00a52a512
sravani-2024 <156399655+sravani-2024@users.noreply.github.com>
2024-05-26 07:38:43 +0530

    Create passwordgenerator.py

---
+++ passwordgenerator.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+print("==<== Welcome to our Random Password Generator ==>==")
+print("==<== ======================================== ==>==")
+
+def main():
+    
+    length=int(input("Enter the length of password you want: "))
+
+    lowerD=string.ascii_lowercase
+    upperD=string.ascii_uppercase
+    digitD=string.digits
+    symbolsD=string.punctuation
+
+    combine=lowerD+upperD+digitD+symbolsD
+    
+    x=random.sample(combine,length)
+    password="".join(x)
+    print(password)
+    main()
+main()
